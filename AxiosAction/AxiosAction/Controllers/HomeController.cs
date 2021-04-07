260008c58e2ad4e91bc1902b30d6a180e6d1ca52
ThanhThuan2k <thuanhuynh.190800@gmail.com>
2021-04-07 19:29:19 +0700

    Create project

---
+++ AxiosAction/AxiosAction/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using AxiosAction.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AxiosAction.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Check(string name)
+		{
+			// insert data to db, but 
+			return Ok(true);
+		}
+	}
+}
